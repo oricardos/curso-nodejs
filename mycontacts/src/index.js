0d512bc12983ad5f88943be42c6784fe00853068
oricardos <ricardo.rs95@gmail.com>
2022-06-21 15:20:28 -0300

    middlewares

--- mycontacts/src/index.js
+++ mycontacts/src/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const routes = require('./routes');
 
 const app = express();
+
+app.use((request, response) => {
+  request.appId = 'MeuAppID';
+  response.send('Intercepted');
+});
 app.use(routes);
 
 app.listen(3000, () => console.log('🔥 Server started at: http://localhost:3000'));
--- mycontacts/src/routes.js
+++ mycontacts/src/routes.js
@@ -4,7 +4,14 @@ const ContactControler = require('./app/controllers/ContactControler');
 
 const router = Router();
 
-router.get('/contacts', ContactControler.index);
+router.get(
+  '/contacts',
+  (request, response) => {
+    request.appId = 'MeuAppID';
+    response.send('Intercepted');
+  },
+  ContactControler.index,
+);
 router.get('/contacts/:id', ContactControler.show);
 router.delete('/contacts/:id', ContactControler.delete);
 
