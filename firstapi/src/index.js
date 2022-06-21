8a7ced64c503b0e5ab8e5387cf25d36929b098eb
oricardos <ricardo.rs95@gmail.com>
2022-06-20 22:49:55 -0300

    feat(Query Params): Recebendo parâmetros na URL

--- firstapi/src/index.js
+++ firstapi/src/index.js
@@ -5,18 +5,30 @@ const routes = require('./routes');
 
 const server = http.createServer((request, response) => {
     const parsedUrl = new URL(`http://localhost:3000${request.url}`)
-
     console.log(`Request url: ${parsedUrl.pathname} Request method: ${request.method}`);
 
+    let { pathname } = parsedUrl;
+    let id = null;
+
+    const splitEndpoint = pathname.split('/').filter(Boolean);
+
+    if (splitEndpoint.length > 1) {
+        pathname = `/${splitEndpoint[0]}/:id`;
+        id = splitEndpoint[1];
+    }
+
     const route = routes.find((routeObj) => (
-        routeObj.endpoint === parsedUrl.pathname && routeObj.method === request.method
+        routeObj.endpoint === pathname && routeObj.method === request.method
     ));
+
+    
 
     if(route) {
         request.query = Object.fromEntries(parsedUrl.searchParams);
+        request.params = { id };
+        
         route.handler(request, response)
     } else {
-        console.log('erro',route);
         response.writeHead(404, {'Content-Type': 'text/html'});
         response.end(`Cannot ${request.method} ${parsedUrl.pathname}`)
     }
