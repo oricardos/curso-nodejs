4a4cc83bbe2316e5b07fcf76deec775c3d0b2a97
oricardos <ricardo.rs95@gmail.com>
2022-06-21 12:09:02 -0300

    configs

--- mycontacts/src/index.js
+++ mycontacts/src/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const app = express();
 
+const app = express();
 app.get('/', (request, response) => {
-    response.send('hll wrld!');
-})
+  response.send('hll wrld!');
+});
 
 app.listen(3000, () => console.log('🔥 Server started at: http://localhost:3000'));
